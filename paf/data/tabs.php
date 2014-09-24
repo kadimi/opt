4a7dff188a865cb66d96982092130db1b4f80c4c
Nabil Kadimi <nabil@kadimi.com>
2014-09-24 09:54:06 +0100

    Minor

--- paf/data/tabs.php
+++ paf/data/tabs.php
@@ -15,14 +15,14 @@ function paf_tabs() {
 	 * Add tabs here
 	 */
 	$tabs[ 'tab_1'] = array(
-		'title' => __( 'Tab one' ),
+		'title'      => __( 'Tab one' ),
 		'menu_title' => __( 'Tab 1' ),
-		'page' => __( 'page_a' ),
+		'page'       => __( 'page_a' ),
 	);
 	$tabs[ 'tab_2'] = array(
-		'title' => __( 'Tab two' ),
+		'title'      => __( 'Tab two' ),
 		'menu_title' => __( 'Tab 2' ),
-		'page' => __( 'page_a' ),
+		'page'       => __( 'page_a' ),
 	);
 
 	/**
