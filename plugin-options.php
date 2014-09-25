8f4c25d92220d7def900a372cbaa5218dcd804b0
Ivan <ichromjak@gmail.com>
2014-09-25 18:10:21 +0200

    initial

---
+++ plugin-options.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: PA Plugin Options
+ * Plugin URI: http://pressapps.co/
+ * Description: Options framework for WordPress plugins
+ * Author: PressApps Team
+ * Text Domain: pressapps
+ * Version: 0.0.1
+ */
