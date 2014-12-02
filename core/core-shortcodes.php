95668aa5ee6f5413792225f30704f5a7944fc76e
Nabil Kadimi <nabil@kadimi.com>
2014-12-02 21:09:47 +0000

    [Incomplete] Add shortcodes with parameters

--- core/core-shortcodes.php
+++ core/core-shortcodes.php
@@ -53,7 +53,7 @@ function skelet_sniff_requests() {
 		}
 		// Show HTML for shortcode popup window
 		header( 'Content-Type: text/html; charset=utf-8');
-		wp_die( call_user_func( 'skelet_' . $serve, $tag ) );
+		die( call_user_func( 'skelet_' . $serve, $tag ) );
 	case 'tinyMCE_js':
 		header( 'Content-Type: application/javascript; charset=utf-8' );
 		die(
@@ -86,24 +86,75 @@ function skelet_tinyMCE_php( $tag ) {
 
 	ob_start();
 
+	// Head
+	printf( '<!DOCTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title></title>' );
+
 	// CSS
+	printf( '<link rel="stylesheet" href="%s" />', admin_url( 'css/common' . ( is_rtl() ? '-rtl' : '' ) . '.css' ) );
 	printf( '<link rel="stylesheet" href="%s" />', admin_url( 'css/forms' . ( is_rtl() ? '-rtl' : '' ) . '.css' ) );
-	print( '<style>'
-		. 'html { background: white; }'
-		. 'body { -webkit-box-shadow: none; box-shadow: none; margin: 0 auto !important; }'
-		. '</style>'
+	printf( '<link rel="stylesheet" href="%s" />', site_url( 'wp-includes/css/buttons' . ( is_rtl() ? '-rtl' : '' ) . '.css' ) );
+	print( '<style>body {
+		height: auto;
+		margin: 0;
+		min-width: 0;
+		padding: 1em;
+	}</style>'
 	);
 
 	// JS
 	printf( '<script src="%s"></script>', "$protocol://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js" );
+	?>
+	<script>
+		var paf;
+		var shortcode = '';
+		jQuery( document ).ready( function ( $ ) {
+			$( 'form' ).submit( function( e ) {
+
+				e.preventDefault();
+
+				// Build the shortcode
+				paf = $( this ).serializeJSON().paf;
+				Object.keys( paf ).map( function(v) {
+					if( 'undefined' !== paf[ v ] && paf[ v ] ) {
+						shortcode += ' '
+							+ v
+							+ '="'
+							+ paf[ v ].toString().split().join()
+							+ '"'
+						;
+					}
+				} );
+				shortcode = "[<?php echo $tag?>" + shortcode + "]";
+
+				// Fill the editor and close
+				parent.tinymce.activeEditor.execCommand( "mceInsertContent", false, shortcode );
+				parent.tinymce.activeEditor.windowManager.close( window );
+			} );
+		} );
+		
+		/*!
+		SerializeJSON jQuery plugin.
+		https://github.com/marioizquierdo/jquery.serializeJSON
+		version 2.4.1 (Oct, 2014)
+		Copyright (c) 2014 Mario Izquierdo
+		Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
+		and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+		*/
+		(function(a){a.fn.serializeJSON=function(k){var g,e,j,h,i,c,d,b;d=a.serializeJSON;b=d.optsWithDefaults(k);d.validateOptions(b);e=this.serializeArray();d.readCheckboxUncheckedValues(e,this,b);g={};a.each(e,function(l,f){j=d.splitInputNameIntoKeysArray(f.name);h=j.pop();if(h!=="skip"){i=d.parseValue(f.value,h,b);if(b.parseWithFunction&&h==="_"){i=b.parseWithFunction(i,f.name)}d.deepSet(g,j,i,b)}});return g};a.serializeJSON={defaultOptions:{parseNumbers:false,parseBooleans:false,parseNulls:false,parseAll:false,parseWithFunction:null,checkboxUncheckedValue:undefined,useIntKeysAsArrayIndex:false},optsWithDefaults:function(c){var d,b;if(c==null){c={}}d=a.serializeJSON;b=d.optWithDefaults("parseAll",c);return{parseNumbers:b||d.optWithDefaults("parseNumbers",c),parseBooleans:b||d.optWithDefaults("parseBooleans",c),parseNulls:b||d.optWithDefaults("parseNulls",c),parseWithFunction:d.optWithDefaults("parseWithFunction",c),checkboxUncheckedValue:d.optWithDefaults("checkboxUncheckedValue",c),useIntKeysAsArrayIndex:d.optWithDefaults("useIntKeysAsArrayIndex",c)}},optWithDefaults:function(c,b){return(b[c]!==false)&&(b[c]!=="")&&(b[c]||a.serializeJSON.defaultOptions[c])},validateOptions:function(d){var b,c;c=["parseNumbers","parseBooleans","parseNulls","parseAll","parseWithFunction","checkboxUncheckedValue","useIntKeysAsArrayIndex"];for(b in d){if(c.indexOf(b)===-1){throw new Error("serializeJSON ERROR: invalid option '"+b+"'. Please use one of "+c.join(","))}}},parseValue:function(g,b,c){var e,d;d=a.serializeJSON;if(b=="string"){return g}if(b=="number"||(c.parseNumbers&&d.isNumeric(g))){return Number(g)}if(b=="boolean"||(c.parseBooleans&&(g==="true"||g==="false"))){return(["false","null","undefined","","0"].indexOf(g)===-1)}if(b=="null"||(c.parseNulls&&g=="null")){return["false","null","undefined","","0"].indexOf(g)!==-1?null:g}if(b=="array"||b=="object"){return JSON.parse(g)}if(b=="auto"){return d.parseValue(g,null,{parseNumbers:true,parseBooleans:true,parseNulls:true})}return g},isObject:function(b){return b===Object(b)},isUndefined:function(b){return b===void 0},isValidArrayIndex:function(b){return/^[0-9]+$/.test(String(b))},isNumeric:function(b){return b-parseFloat(b)>=0},splitInputNameIntoKeysArray:function(c){var e,b,d,h,g;g=a.serializeJSON;h=g.extractTypeFromInputName(c),b=h[0],d=h[1];e=b.split("[");e=a.map(e,function(f){return f.replace(/]/g,"")});if(e[0]===""){e.shift()}e.push(d);return e},extractTypeFromInputName:function(c){var b,d;d=a.serializeJSON;if(b=c.match(/(.*):([^:]+)$/)){var e=["string","number","boolean","null","array","object","skip","auto"];if(e.indexOf(b[2])!==-1){return[b[1],b[2]]}else{throw new Error("serializeJSON ERROR: Invalid type "+b[2]+" found in input name '"+c+"', please use one of "+e.join(", "))}}else{return[c,"_"]}},deepSet:function(c,l,j,b){var k,h,g,i,d,e;if(b==null){b={}}e=a.serializeJSON;if(e.isUndefined(c)){throw new Error("ArgumentError: param 'o' expected to be an object or array, found undefined")}if(!l||l.length===0){throw new Error("ArgumentError: param 'keys' expected to be an array with least one element")}k=l[0];if(l.length===1){if(k===""){c.push(j)}else{c[k]=j}}else{h=l[1];if(k===""){i=c.length-1;d=c[i];if(e.isObject(d)&&(e.isUndefined(d[h])||l.length>2)){k=i}else{k=i+1}}if(e.isUndefined(c[k])){if(h===""){c[k]=[]}else{if(b.useIntKeysAsArrayIndex&&e.isValidArrayIndex(h)){c[k]=[]}else{c[k]={}}}}g=l.slice(1);e.deepSet(c[k],g,j,b)}},readCheckboxUncheckedValues:function(e,d,i){var b,h,g,c,j;if(i==null){i={}}j=a.serializeJSON;b="input[type=checkbox][name]:not(:checked)";h=d.find(b).add(d.filter(b));h.each(function(f,k){g=a(k);c=g.attr("data-unchecked-value");if(c){e.push({name:k.name,value:c})}else{if(!j.isUndefined(i.checkboxUncheckedValue)){e.push({name:k.name,value:i.checkboxUncheckedValue})}}})}}}(window.jQuery||window.Zepto||window.$));
+	</script>
+	<?php
+
+	// Close head
+	print( '</head><body class="wp-core-ui">' );
 
 	// Fields
 	$parameters = $paf_shortcodes[ $tag ]['parameters'];
+	echo '<form action = "">';
 	foreach ( $parameters as $k => $v ) {
 		// Validate title
 		$v[ 'title' ] = k::get_var( 'title', $v, $k );
 		// Print option
-		paf_print_option( 'dummy', $v );
+		paf_print_option( $k, $v );
 		if( 'select' === $v[ 'type' ] ) {
 			printf("\n\n");
 			printf( '<link rel="stylesheet" href="%s" />', "$protocol://cdnjs.cloudflare.com/ajax/libs/select2/3.5.0/select2.min.css" );
@@ -116,8 +167,22 @@ function skelet_tinyMCE_php( $tag ) {
 		}
 	}
 
-	// Remove the error class
+	// Button
+	echo '<hr />';
+	echo '<p>';
+	K::input( 'submit'
+		, array(
+			'class' => 'button button-large button-primary aligncenter	',
+			'type' => 'submit',
+			'value' => 'Add shortcode',
+		)
+	);
+	echo '</p>';
+
+	echo '</form>';
+	echo '</body></html>';
 	return ob_get_clean();
+
 }
 
 // output for "skelet/tinyMCE.js"
