2cb96e55a87c7e67d69dbaa29b3c32fc887d561d
Nabil Kadimi <nabil@kadimi.com>
2019-03-07 13:43:14 +0000

    Create opt.php

---
+++ opt.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+* @file
+* Main file of the "Opt Framework"
+* 
+* The file loads the core files if not already done inside another plugin,
+* then it loads the options defined in the options folder.
+* 
+* @package opt
+*/
+
+/**
+ * Load the framework core if not done inside another plugin
+ */
+if ( ! defined( 'OPT' ) ) {
+	include dirname( __FILE__ ) . '/core/core.php';
+}
+
+/**
+ * Use sample data if $skelet_use_sample_data evaluates to true
+ */
+if ( K::get_var( 'opt_use_sample_data' ) ) {
+	skelet_dir( dirname( __FILE__ ) . '/sample-data/' );
+}
